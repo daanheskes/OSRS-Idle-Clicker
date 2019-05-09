91edfc4771ded18dabcd24dfdd54c9e9a1eda642
daanheskes <daanheskes@hotmail.com>
2019-05-09 11:23:17 +0200

    Fixed App.test.js import

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './Components/App';
 
-it('renders without crashing', () => {
+test('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
