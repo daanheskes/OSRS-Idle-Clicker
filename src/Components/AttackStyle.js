360b218837a08efebd12e32db3fdea282e049a65
daanheskes <daanheskes@hotmail.com>
2019-10-30 00:36:43 +0100

    Added combatstyle images, magic spell images

--- src/Components/AttackStyle.js
+++ src/Components/AttackStyle.js
@@ -5,7 +5,7 @@ class AttackStyle extends Component {
         return(
             <div id='attackStyles'>
                 <div id='attackMethod'>
-                    <img src='https://oldschool.runescape.wiki/images/3/3d/CombatStyles_hacksword.png' alt="Combat styles" />
+                    
                 </div>
             </div>
         );
--- src/Components/Data/MagicSpellsList.js
+++ src/Components/Data/MagicSpellsList.js
@@ -1,142 +1,152 @@
+function importAll(r) {
+    let images = {};
+    r.keys().map((item, index) => {
+        return images[item.replace('./', '')] = r(item);
+    });
+    return images;
+}
+
+const images = importAll(require.context('./../../assets/magic', false, /\.png$/));
+
 export default {
     'windstrike': {
         name: "Wind Strike",
         level: 1,
         damage: 2,
         experience: 5.5,
-        img: "https://oldschool.runescape.wiki/images/1/16/Wind_Strike_icon.png"
+        img: images['Wind_Strike_icon.png']
     },
     'waterstrike': {
         name: "Water Strike",
         level: 5,
         damage: 4,
         experience: 7.5,
-        img: "https://oldschool.runescape.wiki/images/c/cc/Water_Strike_icon.png"
+        img: images['Water_Strike_icon.png']
     },
     'earthstrike': {
         name: "Earth Strike",
         level: 9,
         damage: 6,
         experience: 9.5,
-        img: "https://oldschool.runescape.wiki/images/0/00/Earth_Strike_icon.png"
+        img: images['Earth_Strike_icon.png']
     },
     'firestrike': {
         name: "Fire Strike",
         level: 13,
         damage: 8,
         experience: 11.5,
-        img: "https://oldschool.runescape.wiki/images/0/07/Fire_Strike_icon.png" 
+        img: images['Fire_Strike_icon.png'] 
     },
     'windbolt': {
         name: "Wind Bolt",
         level: 17,
         damage: 9,
         experience: 13.5,
-        img: "https://oldschool.runescape.wiki/images/d/d1/Wind_Bolt_icon.png"
+        img: images['Wind_Bolt_icon.png']
     },
     'waterbolt': {
         name: "Water Bolt",
         level: 23,
         damage: 10,
         experience: 16.5,
-        img: "https://oldschool.runescape.wiki/images/0/00/Water_Bolt_icon.png"
+        img: images['Water_Bolt_icon.png']
     },
     'earthbolt': {
         name: "Earth Bolt",
         level: 29,
         damage: 11,
         experience: 19.5,
-        img: "https://oldschool.runescape.wiki/images/8/8a/Earth_Bolt_icon.png"
+        img: images['Earth_Bolt_icon.png']
     },
     'firebolt': {
         name: "Fire Bolt",
         level: 35,
         damage: 12,
         experience: 22.5,
-        img: "https://oldschool.runescape.wiki/images/2/26/Fire_Bolt_icon.png"
+        img: images['Fire_Bolt_icon.png']
     },
     'windblast': {
         name: "Wind Blast",
         level: 41,
         damage: 13,
         experience: 25.5,
-        img: "https://oldschool.runescape.wiki/images/8/8e/Wind_Blast_icon.png"
+        img: images['Wind_Blast_icon.png']
     },
     'waterblast': {
         name: "Water Blast",
         level: 47,
         damage: 14,
         experience: 28.5,
-        img: "https://oldschool.runescape.wiki/images/5/59/Water_Blast_icon.png"
+        img: images['Water_Blast_icon.png']
     },
     'earthblast': {
         name: "Earth Blast",
         level: 53,
         damage: 15,
         experience: 31.5,
-        img: "https://oldschool.runescape.wiki/images/e/ee/Earth_Blast_icon.png"
+        img: images['Earth_Blast_icon.png']
     },
     'fireblast': {
         name: "Fire Blast",
         level: 59,
         damage: 16,
         experience: 34.5,
-        img: "https://oldschool.runescape.wiki/images/f/ff/Fire_Blast_icon.png"
+        img: images['Fire_Blast_icon.png']
     },
     'windwave': {
         name: "Wind Wave",
         level: 62,
         damage: 17,
         experience: 36,
-        img: "https://oldschool.runescape.wiki/images/e/e0/Wind_Wave_icon.png"
+        img: images['Wind_Wave_icon.png']
     },
     'waterwave': {
         name: "Water Wave",
         level: 65,
         damage: 18,
         experience: 37.5,
-        img: "https://oldschool.runescape.wiki/images/8/8f/Water_Wave_icon.png"
+        img: images['Water_Wave_icon.png']
     },
     'earthwave': {
         name: "Earth Wave",
         level: 70,
         damage: 19,
         experience: 40,
-        img: "https://oldschool.runescape.wiki/images/a/a3/Earth_Wave_icon.png"
+        img: images['Earth_Wave_icon.png']
     },
     'firewave': {
         name: "Fire Wave",
         level: 75,
         damage: 20,
         experience: 42.5,
-        img: "https://oldschool.runescape.wiki/images/f/fd/Fire_Wave_icon.png"
+        img: images['Fire_Wave_icon.png']
     },
     'windsurge': {
         name: "Wind Surge",
         level: 81,
         damage: 21,
         experience: 44.5,
-        img: "https://oldschool.runescape.wiki/images/8/85/Wind_Surge_icon.png"
+        img: images['Wind_Surge_icon.png']
     },
     'watersurge': {
         name: "Water Surge",
         level: 85,
         damage: 22,
         experience: 46.5,
-        img: "https://oldschool.runescape.wiki/images/e/e2/Water_Surge_icon.png"
+        img: images['Water_Surge_icon.png']
     },
     'earthsurge': {
         name: "Earth Surge",
         level: 90,
         damage: 23,
         experience: 48.5,
-        img: "https://oldschool.runescape.wiki/images/a/a0/Earth_Surge_icon.png"
+        img: images['Earth_Surge_icon.png']
     },
     'firesurge': {
         name: "Fire Surge",
         level: 95,
         damage: 24,
         experience: 50.5,
-        img: "https://oldschool.runescape.wiki/images/7/79/Fire_Surge_icon.png"
+        img: images['Fire_Surge_icon.png']
     }
 }
