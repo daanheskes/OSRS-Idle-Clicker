0d53df8e75f4ca99a1055a48890626f8a2d03880
daanheskes <daanheskes@hotmail.com>
2019-11-01 11:18:22 +0100

    Imported all images statically

--- src/Components/Data/EquipmentList.js
+++ src/Components/Data/EquipmentList.js
@@ -1,12 +1,68 @@
-function importAll(r) {
-    let images = {};
-    r.keys().map((item, index) => {
-        return images[item.replace('./', '')] = r(item);
-    });
-    return images;
-}
-
-const images = importAll(require.context('./../../assets/items', false, /\.png$/));
+import bronzefullhelmImage from './../../assets/items/Bronze_full_helm.png';
+import ironfullhelmImage from './../../assets/items/Iron_full_helm.png';
+import steelfullhelmImage from './../../assets/items/Steel_full_helm.png';
+import blackfullhelmImage from './../../assets/items/Black_full_helm.png';
+import mithrilfullhelmImage from './../../assets/items/Mithril_full_helm.png';
+import adamantfullhelmImage from './../../assets/items/Adamant_full_helm.png';
+import runefullhelmImage from './../../assets/items/Rune_full_helm.png';
+import berserkerhelmImage from './../../assets/items/Berserker_helm.png';
+import redcapeImage from './../../assets/items/Red_cape.png';
+import obsidiancapeImage from './../../assets/items/Obsidian_cape.png';
+import firecapeImage from './../../assets/items/Fire_cape.png';
+import amuletofaccuracyImage from './../../assets/items/Amulet_of_accuracy.png';
+import amuletofdefenceImage from './../../assets/items/Amulet_of_defence.png';
+import amuletofstrengthImage from './../../assets/items/Amulet_of_strength.png';
+import amuletofpowerImage from './../../assets/items/Amulet_of_power.png';
+import amuletofgloryImage from './../../assets/items/Amulet_of_glory.png';
+import amuletoffuryImage from './../../assets/items/Amulet_of_fury.png';
+import bronzearrow5Image from './../../assets/items/Bronze_arrow_5.png';
+import ironarrow5Image from './../../assets/items/Iron_arrow_5.png';
+import steelarrow5Image from './../../assets/items/Steel_arrow_5.png';
+import mithrilarrow5Image from './../../assets/items/Mithril_arrow_5.png';
+import adamantarrow5Image from './../../assets/items/Adamant_arrow_5.png';
+import runearrow5Image from './../../assets/items/Rune_arrow_5.png';
+import amethystarrow5Image from './../../assets/items/Amethyst_arrow_5.png';
+import bronzeswordImage from './../../assets/items/Bronze_sword.png';
+import bronzescimitarImage from './../../assets/items/Bronze_scimitar.png';
+import ironscimitarImage from './../../assets/items/Iron_scimitar.png';
+import steelscimitarImage from './../../assets/items/Steel_scimitar.png';
+import blackscimitarImage from './../../assets/items/Black_scimitar.png';
+import mithrilscimitarImage from './../../assets/items/Mithril_scimitar.png';
+import adamantscimitarImage from './../../assets/items/Adamant_scimitar.png';
+import runescimitarImage from './../../assets/items/Rune_scimitar.png';
+import dragonscimitarImage from './../../assets/items/Dragon_scimitar.png';
+import shortbowImage from './../../assets/items/Shortbow.png';
+import bronzeplatebodyImage from './../../assets/items/Bronze_platebody.png';
+import ironplatebodyImage from './../../assets/items/Iron_platebody.png';
+import steelplatebodyImage from './../../assets/items/Steel_platebody.png';
+import blackplatebodyImage from './../../assets/items/Black_platebody.png';
+import mithrilplatebodyImage from './../../assets/items/Mithril_platebody.png';
+import adamantplatebodyImage from './../../assets/items/Adamant_platebody.png';
+import runeplatebodyImage from './../../assets/items/Rune_platebody.png';
+import fightertorsoImage from './../../assets/items/Fighter_torso.png';
+import woodenShield from './../../assets/items/Wooden_shield.png';
+import bronzekiteshieldImage from './../../assets/items/Bronze_kiteshield.png';
+import ironkiteshieldImage from './../../assets/items/Iron_kiteshield.png';
+import steelkiteshieldImage from './../../assets/items/Steel_kiteshield.png';
+import blackkiteshieldImage from './../../assets/items/Black_kiteshield.png';
+import mithrilkiteshieldImage from './../../assets/items/Mithril_kiteshield.png';
+import adamantkiteshieldImage from './../../assets/items/Adamant_kiteshield.png';
+import runekiteshieldImage from './../../assets/items/Rune_kiteshield.png';
+import dragondefenderImage from './../../assets/items/Dragon_defender.png';
+import bronzeplatelegsImage from './../../assets/items/Bronze_platelegs.png';
+import ironplatelegsImage from './../../assets/items/Iron_platelegs.png';
+import steelplatelegsImage from './../../assets/items/Steel_platelegs.png';
+import blackplatelegsImage from './../../assets/items/Black_platelegs.png';
+import mithrilplatelegsImage from './../../assets/items/Mithril_platelegs.png';
+import adamantplatelegsImage from './../../assets/items/Adamant_platelegs.png';
+import runeplatelegsImage from './../../assets/items/Rune_platelegs.png';
+import leatherglovesImage from './../../assets/items/Leather_gloves.png';
+import leatherbootsImage from './../../assets/items/Leather_boots.png';
+import goldringImage from './../../assets/items/Gold_ring.png';
+import warriorringImage from './../../assets/items/Warrior_ring.png';
+import seersringImage from './../../assets/items/Seers_ring.png';
+import archersringImage from './../../assets/items/Archers_ring.png';
+import berserkerringImage from './../../assets/items/Berserker_ring.png';
 
 let equipment = {
     head: {},
@@ -47,92 +103,92 @@ function addItem(slot, name, requirements, cost, img, income=0, atk_bonus=0, str
 }
 
 let currentSlot = 'head';
-addItem(currentSlot, "Bronze full helm", {}, 44, images['Bronze_full_helm.png'], 0, 0, 0, 5);
-addItem(currentSlot, "Iron full helm", {}, 154, images['Iron_full_helm.png'], 0, 0, 0, 7);
-addItem(currentSlot, "Steel full helm", { defence: 5 }, 550, images['Steel_full_helm.png'], 0, 0, 0, 10);
-addItem(currentSlot, "Black full helm", { defence: 10 }, 1056, images['Black_full_helm.png'], 0, 0, 0, 13);
-addItem(currentSlot, "Mithril full helm", { defence: 20 }, 1430, images['Mithril_full_helm.png'], 0, 0, 0, 14);
-addItem(currentSlot, "Adamant full helm", { defence: 30 }, 3520, images['Adamant_full_helm.png'], 0, 0, 0, 21);
-addItem(currentSlot, "Rune full helm", { defence: 40 }, 35200, images['Rune_full_helm.png'], 0, 0, 0, 32);
-addItem(currentSlot, "Berserker helm", { defence: 45 }, 0, images['Berserker_helm.png'], 0, 0, 3, 33);
+addItem(currentSlot, "Bronze full helm", {}, 44, bronzefullhelmImage, 0, 0, 0, 5);
+addItem(currentSlot, "Iron full helm", {}, 154, ironfullhelmImage, 0, 0, 0, 7);
+addItem(currentSlot, "Steel full helm", { defence: 5 }, 550, steelfullhelmImage, 0, 0, 0, 10);
+addItem(currentSlot, "Black full helm", { defence: 10 }, 1056, blackfullhelmImage, 0, 0, 0, 13);
+addItem(currentSlot, "Mithril full helm", { defence: 20 }, 1430, mithrilfullhelmImage, 0, 0, 0, 14);
+addItem(currentSlot, "Adamant full helm", { defence: 30 }, 3520, adamantfullhelmImage, 0, 0, 0, 21);
+addItem(currentSlot, "Rune full helm", { defence: 40 }, 35200, runefullhelmImage, 0, 0, 0, 32);
+addItem(currentSlot, "Berserker helm", { defence: 45 }, 0, berserkerhelmImage, 0, 0, 3, 33);
 
 currentSlot = 'cape';
-addItem(currentSlot, "Red cape", {}, 2, images['Red_cape.png'], 0, 0, 0, 2);
-addItem(currentSlot, "Obsidian cape", {}, 60000, images['Obsidian_cape.png'], 0, 0, 0, 9);
-addItem(currentSlot, "Fire cape", {}, 0, images['Fire_cape.png'], 0, 1, 4, 11, 1, 1);
+addItem(currentSlot, "Red cape", {}, 2, redcapeImage, 0, 0, 0, 2);
+addItem(currentSlot, "Obsidian cape", {}, 60000, obsidiancapeImage, 0, 0, 0, 9);
+addItem(currentSlot, "Fire cape", {}, 0, firecapeImage, 0, 1, 4, 11, 1, 1);
 
 currentSlot = 'neck';
-addItem(currentSlot, "Amulet of accuracy", {}, 1100, images['Amulet_of_accuracy.png'], 0, 4, 0, 0);
-addItem(currentSlot, "Amulet of defence", {}, 1275, images['Amulet_of_defence.png'], 0, 0, 0, 7);
-addItem(currentSlot, "Amulet of strength", {}, 2025, images['Amulet_of_strength.png'], 0, 0, 10);
-addItem(currentSlot, "Amulet of power", {}, 3525, images['Amulet_of_power.png'], 0, 6, 6, 6, 6, 6);
-addItem(currentSlot, "Amulet of glory", {}, 17625, images['Amulet_of_glory.png'], 0, 10, 6, 3, 10, 10);
-addItem(currentSlot, "Amulet of fury", {}, 1200000, images['Amulet_of_fury.png'], 0, 10, 8, 15, 10, 10, 10);
+addItem(currentSlot, "Amulet of accuracy", {}, 1100, amuletofaccuracyImage, 0, 4, 0, 0);
+addItem(currentSlot, "Amulet of defence", {}, 1275, amuletofdefenceImage, 0, 0, 0, 7);
+addItem(currentSlot, "Amulet of strength", {}, 2025, amuletofstrengthImage, 0, 0, 10);
+addItem(currentSlot, "Amulet of power", {}, 3525, amuletofpowerImage, 0, 6, 6, 6, 6, 6);
+addItem(currentSlot, "Amulet of glory", {}, 17625, amuletofgloryImage, 0, 10, 6, 3, 10, 10);
+addItem(currentSlot, "Amulet of fury", {}, 1200000, amuletoffuryImage, 0, 10, 8, 15, 10, 10, 10);
 
 currentSlot = 'ammunition';
-addItem(currentSlot, "Bronze arrow", {}, 1, images['Bronze_arrow.png'], 0, 0, 0, 0, 7);
-addItem(currentSlot, "Iron arrow", {}, 3, images['Iron_arrow.png'], 0, 0, 0, 0, 10);
-addItem(currentSlot, "Steel arrow", {}, 12, images['Steel_arrow.png'], 0, 0, 0, 0, 16);
-addItem(currentSlot, "Mithril arrow", {}, 32, images['Mithril_arrow.png'], 0, 0, 0, 0, 22);
-addItem(currentSlot, "Adamant arrow", {}, 80, images['Adamant_arrow.png'], 0, 0, 0, 0, 31);
-addItem(currentSlot, "Rune arrow", {}, 240, images['Rune_arrow.png'], 0, 0, 0, 0, 49);
-addItem(currentSlot, "Amethyst arrow", {}, 300, images['Amethyst_arrow.png'], 0, 0, 0, 0, 55);
+addItem(currentSlot, "Bronze arrow", {}, 1, bronzearrow5Image, 0, 0, 0, 0, 7);
+addItem(currentSlot, "Iron arrow", {}, 3, ironarrow5Image, 0, 0, 0, 0, 10);
+addItem(currentSlot, "Steel arrow", {}, 12, steelarrow5Image, 0, 0, 0, 0, 16);
+addItem(currentSlot, "Mithril arrow", {}, 32, mithrilarrow5Image, 0, 0, 0, 0, 22);
+addItem(currentSlot, "Adamant arrow", {}, 80, adamantarrow5Image, 0, 0, 0, 0, 31);
+addItem(currentSlot, "Rune arrow", {}, 240, runearrow5Image, 0, 0, 0, 0, 49);
+addItem(currentSlot, "Amethyst arrow", {}, 300, amethystarrow5Image, 0, 0, 0, 0, 55);
 
 currentSlot = 'weapon';
 // Melee
-addItem(currentSlot, "Bronze sword", {}, 26, images['Bronze_sword.png'], 0, 4, 5, 0);
-addItem(currentSlot, "Bronze scimitar", {}, 32, images['Bronze_scimitar.png'], 0, 7, 6, 1);
-addItem(currentSlot, "Iron scimitar", {}, 112, images['Iron_scimitar.png'], 0, 10, 9, 1);
-addItem(currentSlot, "Steel scimitar", { attack: 5 }, 400, images['Steel_scimitar.png'], 0, 15, 14, 1);
-addItem(currentSlot, "Black scimitar", { attack: 10 }, 768, images['Black_scimitar.png'], 0, 19, 14, 1);
-addItem(currentSlot, "Mithril scimitar", { attack: 20 }, 1040, images['Mithril_scimitar.png'], 0, 21, 20, 1);
-addItem(currentSlot, "Adamant scimitar", { attack: 30 }, 2560, images['Adamant_scimitar.png'], 0, 29, 28, 1);
-addItem(currentSlot, "Rune scimitar", { attack: 40 }, 25600, images['Rune_scimitar.png'], 0, 45, 44, 1);
-addItem(currentSlot, "Dragon scimitar", { attack: 60 }, 100000, images['Dragon_scimitar.png'], 0, 67, 66, 1);
+addItem(currentSlot, "Bronze sword", {}, 26, bronzeswordImage, 0, 4, 5, 0);
+addItem(currentSlot, "Bronze scimitar", {}, 32, bronzescimitarImage, 0, 7, 6, 1);
+addItem(currentSlot, "Iron scimitar", {}, 112, ironscimitarImage, 0, 10, 9, 1);
+addItem(currentSlot, "Steel scimitar", { attack: 5 }, 400, steelscimitarImage, 0, 15, 14, 1);
+addItem(currentSlot, "Black scimitar", { attack: 10 }, 768, blackscimitarImage, 0, 19, 14, 1);
+addItem(currentSlot, "Mithril scimitar", { attack: 20 }, 1040, mithrilscimitarImage, 0, 21, 20, 1);
+addItem(currentSlot, "Adamant scimitar", { attack: 30 }, 2560, adamantscimitarImage, 0, 29, 28, 1);
+addItem(currentSlot, "Rune scimitar", { attack: 40 }, 25600, runescimitarImage, 0, 45, 44, 1);
+addItem(currentSlot, "Dragon scimitar", { attack: 60 }, 100000, dragonscimitarImage, 0, 67, 66, 1);
 // Ranged
-addItem(currentSlot, "Shortbow", {}, 50, images['Shortbow.png'], 0, 0, 0, 0, 8)
+addItem(currentSlot, "Shortbow", {}, 50, shortbowImage, 0, 0, 0, 0, 8)
 
 currentSlot = 'body';
-addItem(currentSlot, "Bronze platebody", {}, 160, images['Bronze_platebody.png'], 0, 0, 0, 15);
-addItem(currentSlot, "Iron platebody", {}, 560, images['Iron_platebody.png'], 0, 0, 0, 21);
-addItem(currentSlot, "Steel platebody", { defence: 5 }, 2000, images['Steel_platebody.png'], 0, 0, 0, 32);
-addItem(currentSlot, "Black platebody", { defence: 10 }, 3840, images['Black_platebody.png'], 0, 0, 0, 41);
-addItem(currentSlot, "Mithril platebody", { defence: 20 }, 5200, images['Mithril_platebody.png'], 0, 0, 0, 46);
-addItem(currentSlot, "Adamant platebody", { defence: 30 }, 16640, images['Adamant_platebody.png'], 0, 0, 0, 65);
-addItem(currentSlot, "Rune platebody", { defence: 40 }, 65000, images['Rune_platebody.png'], 0, 0, 0, 82);
-addItem(currentSlot, "Fighter torso", { defence: 40 }, 0, images['Fighter_torso.png'], 0, 0, 4, 85);
+addItem(currentSlot, "Bronze platebody", {}, 160, bronzeplatebodyImage, 0, 0, 0, 15);
+addItem(currentSlot, "Iron platebody", {}, 560, ironplatebodyImage, 0, 0, 0, 21);
+addItem(currentSlot, "Steel platebody", { defence: 5 }, 2000, steelplatebodyImage, 0, 0, 0, 32);
+addItem(currentSlot, "Black platebody", { defence: 10 }, 3840, blackplatebodyImage, 0, 0, 0, 41);
+addItem(currentSlot, "Mithril platebody", { defence: 20 }, 5200, mithrilplatebodyImage, 0, 0, 0, 46);
+addItem(currentSlot, "Adamant platebody", { defence: 30 }, 16640, adamantplatebodyImage, 0, 0, 0, 65);
+addItem(currentSlot, "Rune platebody", { defence: 40 }, 65000, runeplatebodyImage, 0, 0, 0, 82);
+addItem(currentSlot, "Fighter torso", { defence: 40 }, 0, fightertorsoImage, 0, 0, 4, 85);
 
 currentSlot = 'shield';
-addItem(currentSlot, "Wooden shield", {}, 20, images['Wooden_shield.png'], 0, 0, 0, 5);
-addItem(currentSlot, "Bronze kiteshield", {}, 68, images['Bronze_kiteshield.png'], 0, 0, 0, 7);
-addItem(currentSlot, "Iron kiteshield", {}, 238, images['Iron_kiteshield.png'], 0, 0, 0, 10);
-addItem(currentSlot, "Steel kiteshield", { defence: 5 }, 850, images['Steel_kiteshield.png'], 0, 0, 0, 15);
-addItem(currentSlot, "Black kiteshield", { defence: 10 }, 1632, images['Black_kiteshield.png'], 0, 0, 0, 19);
-addItem(currentSlot, "Mithril kiteshield", { defence: 20 }, 2210, images['Mithril_kiteshield.png'], 0, 0, 0, 22);
-addItem(currentSlot, "Adamant kiteshield", { defence: 30 }, 5440, images['Adamant_kiteshield.png'], 0, 0, 0, 31);
-addItem(currentSlot, "Rune kiteshield", { defence: 40 }, 54400, images['Rune_kiteshield.png'], 0, 0, 0, 48);
-addItem(currentSlot, "Dragon defender", { defence: 60 }, 0, images['Dragon_defender.png'], 0, 25, 6, 25);
+addItem(currentSlot, "Wooden shield", {}, 20, woodenShield, 0, 0, 0, 5);
+addItem(currentSlot, "Bronze kiteshield", {}, 68, bronzekiteshieldImage, 0, 0, 0, 7);
+addItem(currentSlot, "Iron kiteshield", {}, 238, ironkiteshieldImage, 0, 0, 0, 10);
+addItem(currentSlot, "Steel kiteshield", { defence: 5 }, 850, steelkiteshieldImage, 0, 0, 0, 15);
+addItem(currentSlot, "Black kiteshield", { defence: 10 }, 1632, blackkiteshieldImage, 0, 0, 0, 19);
+addItem(currentSlot, "Mithril kiteshield", { defence: 20 }, 2210, mithrilkiteshieldImage, 0, 0, 0, 22);
+addItem(currentSlot, "Adamant kiteshield", { defence: 30 }, 5440, adamantkiteshieldImage, 0, 0, 0, 31);
+addItem(currentSlot, "Rune kiteshield", { defence: 40 }, 54400, runekiteshieldImage, 0, 0, 0, 48);
+addItem(currentSlot, "Dragon defender", { defence: 60 }, 0, dragondefenderImage, 0, 25, 6, 25);
 
 currentSlot = 'legs';
-addItem(currentSlot, "Bronze platelegs", {}, 80, images['Bronze_platelegs.png'], 0, 0, 0, 8);
-addItem(currentSlot, "Iron platelegs", {}, 280, images['Iron_platelegs.png'], 0, 0, 0, 11);
-addItem(currentSlot, "Steel platelegs", { defence: 5 }, 1000, images['Steel_platelegs.png'], 0, 0, 0, 17);
-addItem(currentSlot, "Black platelegs", { defence: 10 }, 1920, images['Black_platelegs.png'], 0, 0, 0, 21);
-addItem(currentSlot, "Mithril platelegs", { defence: 20 }, 2600, images['Mithril_platelegs.png'], 0, 0, 0, 24);
-addItem(currentSlot, "Adamant platelegs", { defence: 30 }, 6400, images['Adamant_platelegs.png'], 0, 0, 0, 33);
-addItem(currentSlot, "Rune platelegs", { defence: 40 }, 64000, images['Rune_platelegs.png'], 0, 0, 0, 51);
+addItem(currentSlot, "Bronze platelegs", {}, 80, bronzeplatelegsImage, 0, 0, 0, 8);
+addItem(currentSlot, "Iron platelegs", {}, 280, ironplatelegsImage, 0, 0, 0, 11);
+addItem(currentSlot, "Steel platelegs", { defence: 5 }, 1000, steelplatelegsImage, 0, 0, 0, 17);
+addItem(currentSlot, "Black platelegs", { defence: 10 }, 1920, blackplatelegsImage, 0, 0, 0, 21);
+addItem(currentSlot, "Mithril platelegs", { defence: 20 }, 2600, mithrilplatelegsImage, 0, 0, 0, 24);
+addItem(currentSlot, "Adamant platelegs", { defence: 30 }, 6400, adamantplatelegsImage, 0, 0, 0, 33);
+addItem(currentSlot, "Rune platelegs", { defence: 40 }, 64000, runeplatelegsImage, 0, 0, 0, 51);
 
 currentSlot = 'hand';
-addItem(currentSlot, "Leather gloves", {}, 6, images['Leather_gloves.png'], 0, 0, 0, 2);
+addItem(currentSlot, "Leather gloves", {}, 6, leatherglovesImage, 0, 0, 0, 2);
 
 currentSlot = 'feet';
-addItem(currentSlot, "Leather boots", {}, 6, images['Leather_boots.png'], 0, 0, 0, 1);
+addItem(currentSlot, "Leather boots", {}, 6, leatherbootsImage, 0, 0, 0, 1);
 
 currentSlot = 'ring';
-addItem(currentSlot, "Gold ring", {}, 210, images['Gold_ring.png'], 1, 0, 0, 0);
-addItem(currentSlot, "Warrior ring", {}, 100000, images['Warrior_ring.png'], 0, 4, 0, 4);
-addItem(currentSlot, "Seers ring", {}, 1000000, images['Seers_ring.png'], 0, 0, 0, 4, 0, 4);
-addItem(currentSlot, "Archers ring", {}, 1000000, images['Archers_ring.png'], 0, 0, 0, 4, 4);
-addItem(currentSlot, "Berserker ring", {}, 1000000, images['Berserker_ring.png'], 0, 0, 4, 4);
+addItem(currentSlot, "Gold ring", {}, 210, goldringImage, 1, 0, 0, 0);
+addItem(currentSlot, "Warrior ring", {}, 100000, warriorringImage, 0, 4, 0, 4);
+addItem(currentSlot, "Seers ring", {}, 1000000, seersringImage, 0, 0, 0, 4, 0, 4);
+addItem(currentSlot, "Archers ring", {}, 1000000, archersringImage, 0, 0, 0, 4, 4);
+addItem(currentSlot, "Berserker ring", {}, 1000000, berserkerringImage, 0, 0, 4, 4);
 
 export default equipment;
--- src/Components/Data/MonsterList.js
+++ src/Components/Data/MonsterList.js
@@ -1,18 +1,23 @@
-function importAll(r) {
-	let images = {};
-	r.keys().map((item, index) => {
-		return images[item.replace('./', '')] = r(item);
-	});
-	return images;
-}
-
-const images = importAll(require.context('./../../assets/monsters', false, /\.png$/));
+import chickenImage from './../../assets/monsters/Chicken.png';
+import goblinImage from './../../assets/monsters/Goblin.png';
+import cowCalfImage from './../../assets/monsters/Cow_calf.png';
+import cowImage from './../../assets/monsters/Cow.png';
+import highwaymanImage from './../../assets/monsters/Highwayman.png';
+import darkwizardImage from './../../assets/monsters/Dark_wizard.png';
+import dwarfImage from './../../assets/monsters/Dwarf.png';
+import alkharidwarriorImage from './../../assets/monsters/Al-Kharid_warrior.png';
+import wolfImage from './../../assets/monsters/Wolf.png';
+import minotaurImage from './../../assets/monsters/Minotaur.png';
+import scorpionImage from './../../assets/monsters/Scorpion.png';
+import guardImage from './../../assets/monsters/Guard.png';
+import hillgiantImage from './../../assets/monsters/Hill_giant.png';
+import mossgiantImage from './../../assets/monsters/Moss_giant.png';
 
 export default {
 	chicken: {
 		name: "Chicken",
 		combatlevel: 1,
-		img: images['Chicken.png'],
+		img: chickenImage,
 		hitpoints: 3,
 		bones: "Bones"
 	},
@@ -20,91 +25,91 @@ export default {
 		name: "Goblin",
 		combatlevel: 2,
 		hitpoints: 5,
-		img: images['Goblin.png'],
+		img: goblinImage,
 		bones: "Bones"
 	},
 	cowcalf: {
 		name: "Cow calf",
 		combatlevel: 2,
 		hitpoints: 6,
-		img: images['Cow_calf.png'],
+		img: cowCalfImage,
 		bones: "Bones"
 	},
 	cow: {
 		name: "Cow",
 		combatlevel: 2,
 		hitpoints: 8,
-		img: images['Cow.png'],
+		img: cowImage,
 		bones: "Bones"
 	},
 	highwayman: {
 		name: "Highwayman",
 		combatlevel: 5,
 		hitpoints: 13,
-		img: images['Highwayman.png'],
+		img: highwaymanImage,
 		bones: "Bones"
 	},
 	darkwizard: {
 		name: "Dark wizard",
 		combatlevel: 7,
 		hitpoints: 12,
-		img: images['Dark_wizard.png'],
+		img: darkwizardImage,
 		bones: "Bones"
 	},
 	dwarf: {
 		name: "Dwarf",
 		combatlevel: 10,
 		hitpoints: 16,
-		img: images['Dwarf.png'],
+		img: dwarfImage,
 		bones: "Bones"
 	},
 	alkharidwarrior: {
 		name: "Al-Kharid warrior",
 		combatlevel: 9,
 		hitpoints: 19,
-		img: images['Al-Kharid_warrior.png'],
+		img: alkharidwarriorImage,
 		bones: "Bones"
 	},
 	wolf: {
 		name: "Wolf",
 		combatlevel: 11,
 		hitpoints: 10,
-		img: images['Wolf.png'],
+		img: wolfImage,
 		bones: "Bones"
 	},
 	minotaur: {
 		name: "Minotaur",
 		combatlevel: 12,
 		hitpoints: 10,
-		img: images['Minotaur.png'],
+		img: minotaurImage,
 		bones: "Bones"
 	},
 	scorpion: {
 		name: "Scorpion",
 		combatlevel: 14,
 		hitpoints: 17,
-		img: images['Scorpion.png'],
+		img: scorpionImage,
 		bones: "Bones"
 	},
 	guard: {
 		name: "Guard",
 		combatlevel: 21,
 		hitpoints: 22,
-		img: images['Guard.png'],
+		img: guardImage,
 		bones: "Bones"
 	},
 	hillgiant: {
 		name: "Hill Giant",
 		combatlevel: 28,
 		hitpoints: 35,
-		img: images['Hill_giant.png'],
+		img: hillgiantImage,
 		bones: "Big bones"
 	},
 	mossgiant: {
 		name: "Moss Giant",
 		combatlevel: 42,
 		hitpoints: 60,
-		img: images['Moss_giant.png'],
+		img: mossgiantImage,
 		bones: "Big bones"
 	}
 }
