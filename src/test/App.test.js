7b3a81a14afa20dab944d2dde5355de6fdfffe61
daanheskes <daanheskes@hotmail.com>
2019-12-12 12:04:26 +0100

    Fixed tests

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from '../Components/App';
+import App from '../Components/App.js';
 
 test('renders without crashing', () => {
   const div = document.createElement('div');
