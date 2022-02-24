411963631ced09ba278a5efc40a02becba356730
kaidev <kaimetaverse@protonmail.com>
2022-02-25 04:52:54 +1000

    dev
    dev

--- src/Data/PrayerExperience.js
+++
@@ -1,9 +0,0 @@
-export default {
-  "Bones": 4.5,
-	"Big bones": 15,
-	"Baby dragon bones": 30,
-	"Dragon bones": 72,
-	"Wyvern bones": 72,
-	"Lava dragon bones": 85,
-	"Superior dragon bones": 150
-}
